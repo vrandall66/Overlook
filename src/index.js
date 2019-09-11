2ece2d53a2d8e846336f31ef8c9e72c0e98904d7
Vanessa Randall <vanessalrandall@gmail.com>
2019-09-11 15:37:04 -0600

    Fetch data

--- src/index.js
+++ src/index.js
@@ -11,3 +11,34 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+let users, rooms, bookings, roomServices;
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
+  .then(data => data.json())
+  .then(data => users = data.users)
+  .catch(err => console.log('users err', err));
+
+setTimeout(() => console.log('users', users), 1000);
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
+  .then(data => data.json())
+  .then(data => rooms = data.rooms)
+  .catch(err => console.log('rooms err', err));
+
+setTimeout(() => console.log('rooms', rooms), 1000);
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
+  .then(data => data.json())
+  .then(data => bookings = data.bookings)
+  .catch(err => console.log('bookings', err));
+
+setTimeout(() => console.log('bookings', bookings), 1000);
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices")
+  .then(data => data.json())
+  .then(data => roomServices = data.roomServices)
+  .catch(err => console.log('roomServices err', err));
+
+setTimeout(() => console.log('roomServices', roomServices), 1000);

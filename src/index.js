2d4cab919ec811b3876d8ee3e866a009f6be7e78
Vanessa Randall <vanessalrandall@gmail.com>
2019-09-11 17:05:19 -0600

    Make test files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 

8afa9d4436c0873bf50d9e202b3dcf2d9af345e9
Vanessa Randall <vanessalrandall@gmail.com>
2019-09-15 10:19:09 -0600

    Fix test issues and find user by name

--- src/Guest.js
+++ src/Guest.js
@@ -5,6 +5,13 @@ class Guest {
     this.data = data;
     this.name = name;
   }
+
+  findUser(name) {
+    let guestIndex = Object.keys(this.data).find(user => {
+      return this.data[user].name === name;
+    });
+    return parseInt(guestIndex) + 1;
+  }
 }
 
 export default Guest;
